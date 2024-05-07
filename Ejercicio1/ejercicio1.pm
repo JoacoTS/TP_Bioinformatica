b7a12744712a5793edbb18fe3c8abbc7d13a9e83
JoacoTS <joacotruccos@gmail.com>
2024-05-06 23:09:07 -0300

    cambio gen y ejercicio 1

--- Ejercicio1/ejercicio1.pm
+++ Ejercicio1/ejercicio1.pm
@@ -1,16 +1,18 @@
 use Bio::DB::GenBank;
 use Bio::SeqIO;
 
-$rna_id = '723961';
+$rna_id = '3630';
 
 $db_obj = Bio::DB::GenBank->new();
 $seq_obj = $db_obj->get_Seq_by_gi($rna_id);
 
+$prot_obj = $seq_obj->translate;
+
 $file = Bio::SeqIO->new(-file => '>mrna.fasta', -format => 'fasta');
-$file->write_seq($seq_obj);
+$file->write_seq($prot_obj);
 
 $file_copy = Bio::SeqIO->new(-file => '>../Ejercicio2/mrna.fasta', -format => 'fasta');
-$file_copy->write_seq($seq_obj);
+$file_copy->write_seq($prot_obj);
 
 $file_copy = Bio::SeqIO->new(-file => '>../Ejercicio2(remoto)/mrna.fasta', -format => 'fasta');
-$file_copy->write_seq($seq_obj);
+$file_copy->write_seq($prot_obj);
